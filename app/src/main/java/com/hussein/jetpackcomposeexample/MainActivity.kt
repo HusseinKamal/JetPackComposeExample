d405920a4e649181f8fe75645324db810033ffb8
husseinkamal <hussain@aqwas.sa>
2023-06-03 00:56:42 +0300

    App mvvm architecture with files - Splash Screen code

--- app/src/main/java/com/hussein/jetpackcomposeexample/MainActivity.kt
+++ app/src/main/java/com/hussein/jetpackcomposeexample/MainActivity.kt
@@ -3,6 +3,7 @@ package com.hussein.jetpackcomposeexample
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -10,11 +11,19 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.hussein.jetpackcomposeexample.ui.theme.JetPackComposeExampleTheme
+import com.hussein.jetpackcomposeexample.viewmodel.SplashViewModel
 
 class MainActivity : ComponentActivity() {
+
+    private val viewModel: SplashViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
+        splashScreen.setKeepOnScreenCondition{viewModel.isLoading.value}
+
         setContent {
             JetPackComposeExampleTheme {
                 // A surface container using the 'background' color from the theme
